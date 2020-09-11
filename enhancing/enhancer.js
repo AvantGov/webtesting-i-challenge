0444ef2e007f5d0e92ba8c666a60ac6288226b57
avantgov <alexcouts18@gmail.com>
2020-09-10 19:22:16 -0500

    completed MVP code with four passing tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,42 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return newItem = {
+      ...item, 
+      enhancement: item.enhancement + 1
+    }
+  } else {
+    return item
+  }
+
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return newItem = {
+      ...item, 
+      durability: item.durability - 5
+    } 
+  } else if (item.enhancement >= 16 ) {
+    return newItem = {
+      ...item, 
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    }
+  } else if (item.enhancement >= 15) {
+    return newItem = {
+      ...item, 
+      durability: item.durability - 10
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return newItem = { 
+    ...item,
+    durability: 100
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const player = {
+    name: "johndoe",
+    durability: 50,
+    enhancement: 5,
+}
+
+const player_at_15 = {
+    name: "at 15 enhance",
+    durability: 50,
+    enhancement: 15
+}
+
+const player_at_17 = {
+    name: "at 17 enhance",
+    durability: 50,
+    enhancement: 17
+}
+
+
+describe("enhancement tests" ,() => {
+    it("repairs the item, replenishing durability", () => {
+        expect(enhancer.repair(player)).toEqual({
+            ...player,
+            durability: 100
+        })
+    })
+
+    it("peforms a success", () => {
+        expect(enhancer.success(player)).toEqual({
+            ...player,
+            enhancement: player.enhancement + 1
+        })
+    })
+
+    it("performs an action", () => {
+        expect(enhancer.fail(player)).toEqual({
+            ...player,
+            durability: player.durability - 5
+        })
+        expect(enhancer.fail(player_at_15)).toEqual({
+            ...player_at_15,
+            durability: 40,
+        
+        })        
+        expect(enhancer.fail(player_at_17)).toEqual({
+            ...player_at_17,
+            durability: 40,
+            enhancement: 16
+        })
+
+    })
+
+})
